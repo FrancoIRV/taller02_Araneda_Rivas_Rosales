b628ff7bd077560c20ebbc8deb086eddf63b1d03
HectorRosales <h.rosales01@ufromail.cl>
2024-10-11 10:15:22 -0300

    Avanze 2 del codigo

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,51 +7,76 @@ public class Main {
     }
 
     public static void opcionesMenu() {
-        System.out.println("Opciones:");
-        System.out.println("1. Ocupar habitación");
-        System.out.println("2. Reiniciar hotel");
-        System.out.println("");
-    }
-
-    public static void ejecutarMenu(String[] habitaciones) {
-        opcionesMenu();
-        Scanner scanner = new Scanner(System.in);
-        int seleccion = scanner.nextInt();
-
-        switch (seleccion) {
-            case 1:
-                ocuparHabitacion(habitaciones);
-                break;
-            case 2:
-                reiniciarHotel(habitaciones);
-                break;
-            default:
-                System.out.println("Selección inválida.");
-                break;
-        }
-
-
+        System.out.println("--------MENU--------");
+        System.out.println("1. Realizar reserva");
+        System.out.println("2. Ocupar habitacion ");
+        System.out.println("3. Vaciar habitacion");
+        System.out.println("4. Imprimir boleta");
     }
 
     public static void run() {
         String[] listaHabitaciones = {"S", "S", "S", "D", "D", "D", "D", "D", "D", "D"};
-        ejecutarMenu(listaHabitaciones);
+        int [] diasReservados = {0,0,0,0,0,0,0,0,0,0};
+        ejecutarMenu(listaHabitaciones,diasReservados);
     }
 
-    public static void ocuparHabitacion(String[] habitaciones) {
+    public static void ejecutarMenu(String[] habitaciones, int [] diasReservados) {
+        opcionesMenu();
+        int valor = pedirOpcionMenu();
 
+        switch (valor){
+            case 1:
+                //relizarreserva()
+            case 2:
+                //ocuparhabitacion
+            case 3:
+                vaciarHabitacion(habitaciones,diasReservados,2);
+            case 4:
+                //realizar reserva
+        }
+    }
+
+    public static int pedirOpcionMenu(){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("ingrese una opcion");
+            return sc.nextInt();
+        } catch (Exception e){
+            System.out.println("Error");
+        }
+    }
+
+
+    public static int getIndex(String[] habitaciones, String diasReservados) {
+
+        int index = -1;
+        for (int i = 0; i < habitaciones.length; i++) {
+            if (habitaciones[i].equals(diasReservados)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    static void vaciarHabitacion(String [] habitaciones,int [] diasReservados, int nro){
+        habitaciones[nro-1] = "D";
+        diasReservados[nro-1] = 0;
+        System.out.println("Habitacion vaciada");
     }
 
     public static void reiniciarHotel(String[] habitaciones) {
+
         for (int i = 0; i < habitaciones.length; i++) {
-            habitaciones[i] = (i < 3) ? "S" : "D";
+            habitaciones[i] = "D";
         }
         System.out.println("Hotel reiniciado.");
     }
 
-    public static void mostrarHabitaciones(String[] habitaciones) {
+    public static void mostrarHabitaciones(String [] habitaciones){
+
         for (int i = 0; i < habitaciones.length; i++) {
-            System.out.println("Habitación " + i + ": " + habitaciones[i]);
+            System.out.println("habitaciones: "+ habitaciones[i]);
         }
     }
 }
